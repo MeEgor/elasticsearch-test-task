f519b5ac041bfb6bb7e2f325d97869f184e62573
Egor Konnov <egorknv@gmail.com>
2015-07-07 15:25:10 +0300

    seed generation script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# populate generator
+def gc i
+  chars = %w{a b c d e f g h i j k l m n o p q r s t w v x y z}
+  i >= 0 ? chars[i] : ''
+end
+
+def nts i
+  i < 10 ? "0#{ i }" : i.to_s
+end
+
+def generate_article i
+  "#{ gc(i / 99) }-#{ gc(i / 99 - 1)  }#{ nts(i % 99) }".upcase
+end
+
+300.times do |i|
+  article = generate_article i
+  params = {
+    article: article,
+    name: "Name of #{ article }".delete('-')
+  }
+  Good.create params
+end
